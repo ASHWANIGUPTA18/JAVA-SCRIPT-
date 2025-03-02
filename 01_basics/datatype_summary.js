9e44df67cfd67ae39680aa835b1e15483e9ac5dc
ASHWANIGUPTA18 <ashwanig2004@gmail.com>
2025-03-02 15:24:08 +0530

    datatypr summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,14 @@
+//primitive
+// 7 types of primitive data types
+// 1. Number    
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+// Non-primitive
+// 1. Object
+// 2. Array
+// 3. Function
